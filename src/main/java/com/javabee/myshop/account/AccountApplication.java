bc71e90551e3547e102fdc174fb19671b22d7288
jsharief <jalaludeen.h@gmail.com>
2021-09-19 20:28:30 -0400

    test commit from vscode.

--- src/main/java/com/javabee/myshop/account/AccountApplication.java
+++ src/main/java/com/javabee/myshop/account/AccountApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/**
+ * Main class for the Account microservices.
+ * 
+ */
 public class AccountApplication {
 
 	public static void main(String[] args) {
